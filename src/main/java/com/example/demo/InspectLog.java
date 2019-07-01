8c7fb388218d4a8b9070b87f706d3af5d393b4f0
韩要翔 <hanyaoxiang@9fbank.com.cn>
2019-07-01 22:29:26 +0800

    disable btrace

--- src/main/java/com/example/demo/InspectLog.java
+++ src/main/java/com/example/demo/InspectLog.java
@@ -7,32 +7,32 @@ import static com.sun.btrace.BTraceUtils.jstack;
 import static com.sun.btrace.BTraceUtils.println;
 import static com.sun.btrace.BTraceUtils.str;
 
-@BTrace
-public class InspectLog {
-    private static long s_1 = 1000 * 1000 * 1000;
-
-    @OnMethod(
-            clazz = "org.apache.ibatis.executor.SimpleExecutor",
-            method = "doQuery",
-            location = @Location(Kind.RETURN)
-    )
-    public static void doQuery(@ProbeMethodName String probeMethod, @Duration long duration) {
-        if (duration > s_1) {
-            println(strcat(probeMethod + "：", str(duration / 1000000)));
-        }
-    }
-
-
-    @OnMethod(
-            clazz = "com.alibaba.druid.pool.DruidPooledPreparedStatement",
-            method = "execute",
-            location = @Location(Kind.RETURN)
-    )
-    public static void execTime(@ProbeMethodName String probeMethod, @Duration long duration) {
-        if (duration > s_1/2) {
-            println(strcat(probeMethod + "：", str(duration / 1000000)));
-        }
-    }
-
-
-}
+//@BTrace
+//public class InspectLog {
+//    private static long s_1 = 1000 * 1000 * 1000;
+//
+//    @OnMethod(
+//            clazz = "org.apache.ibatis.executor.SimpleExecutor",
+//            method = "doQuery",
+//            location = @Location(Kind.RETURN)
+//    )
+//    public static void doQuery(@ProbeMethodName String probeMethod, @Duration long duration) {
+//        if (duration > s_1) {
+//            println(strcat(probeMethod + "：", str(duration / 1000000)));
+//        }
+//    }
+//
+//
+//    @OnMethod(
+//            clazz = "com.alibaba.druid.pool.DruidPooledPreparedStatement",
+//            method = "execute",
+//            location = @Location(Kind.RETURN)
+//    )
+//    public static void execTime(@ProbeMethodName String probeMethod, @Duration long duration) {
+//        if (duration > s_1/2) {
+//            println(strcat(probeMethod + "：", str(duration / 1000000)));
+//        }
+//    }
+//
+//
+//}
